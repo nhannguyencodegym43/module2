6527dab09b213e33e740e769b4fee17e1e69514c
trungnhan_nguyen <trungnhan_nguyen@datahouse.com>
2025-03-17 16:01:06 +0700

    Practice on variable, data type and operator

---
+++ variable_datatype_operator/th/th2/index.js
@@ -0,0 +1,10 @@
+let inputWidth = prompt('Width?');
+let inputHeight = prompt('Height?');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write('The width is: ' + width);
+document.write('<br>');
+document.write('The height is: ' + height);
+document.write('<br>');
+document.write('The area is: ' + area);
